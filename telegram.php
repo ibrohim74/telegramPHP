ff079f18a964a83654727dfbd0e21d73c04cf44b
ibrohim74 <xibroxim@gmail.com>
2024-09-08 01:11:54 +0500

    tg

---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$token = "TOKEN";
+$chat_id = "CHAT_ID";
+
+$arr = array(
+  'Ism: ' => $name,
+  'Telefon nomer: ' => $phone,
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+
+if ($sendToTelegram) {
+  header('Location: /');
+} else {
+  echo "Error";
+}
+?>
